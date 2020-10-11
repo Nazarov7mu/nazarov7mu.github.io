fa9d62ac5660ba3b7d43a7e6649833dfe7514bbc
nazarov7mu <nazarov7mu@gmail.com>
2020-10-11 20:52:48 +0900

    Website V1.0 done

---
+++ assets/js/active_dodgers.js
@@ -0,0 +1,128 @@
+(function ($) {
+    'use strict';
+
+    var browserWindow = $(window);
+
+    // :: 5.0 Sliders Active Code
+    if ($.fn.owlCarousel) {
+        // var welcomeSlide = $('.hero-post-slides');
+        var popularSlide = $('.dodgers-game-slideshow');
+
+        // welcomeSlide.owlCarousel({
+        //     // items: 1,
+        //     // margin: 0,
+        //     loop: true,
+        //     nav: true,
+        //     navText: ['Prev Game', 'Next Game'],
+        //     dots: false,
+        //     autoplay: true,
+        //     autoplayTimeout: 7000,
+        //     smartSpeed: 1000
+        // });
+
+        // welcomeSlide.on('translate.owl.carousel', function () {
+        //     var slideLayer = $("[data-animation]");
+        //     slideLayer.each(function () {
+        //         var anim_name = $(this).data('animation');
+        //         $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+        //     });
+        // });
+
+        // welcomeSlide.on('translated.owl.carousel', function () {
+        //     var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
+        //     slideLayer.each(function () {
+        //         var anim_name = $(this).data('animation');
+        //         $(this).addClass('animated ' + anim_name).css('opacity', '1');
+        //     });
+        // });
+
+        // $("[data-delay]").each(function () {
+        //     var anim_del = $(this).data('delay');
+        //     $(this).css('animation-delay', anim_del);
+        // });
+
+        // $("[data-duration]").each(function () {
+        //     var anim_dur = $(this).data('duration');
+        //     $(this).css('animation-duration', anim_dur);
+        // });
+
+        popularSlide.owlCarousel({
+            items: 1,
+            //margin: 30,
+            center: true,
+            nav: true,
+            navText: ['<ion-icon name="arrow-back-outline"></ion-icon>', '<ion-icon name="arrow-forward-outline"></ion-icon>'],
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 7000,
+            smartSpeed: 1000,
+            // responsive: {
+            //     0: {
+            //         items: 2
+            //     },
+            //     768: {
+            //         items: 3
+            //     },
+            //     992: {
+            //         items: 4
+            //     },
+            //     1200: {
+            //         items: 6
+            //     }
+            // }
+        });
+
+        latestSlide.owlCarousel({
+            items: 1,
+            //margin: 30,
+            center: true,
+            nav: true,
+            navText: ['<ion-icon name="arrow-back-outline"></ion-icon>', '<ion-icon name="arrow-forward-outline"></ion-icon>'],
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 7000,
+            smartSpeed: 1000,
+            // responsive: {
+            //     0: {
+            //         items: 2
+            //     },
+            //     768: {
+            //         items: 3
+            //     },
+            //     992: {
+            //         items: 4
+            //     },
+            //     1200: {
+            //         items: 6
+            //     }
+            // }
+        });
+
+        editorSlide.owlCarousel({
+            items: 1,
+            //margin: 30,
+            center: true,
+            nav: true,
+            navText: ['<ion-icon name="arrow-back-outline"></ion-icon>', '<ion-icon name="arrow-forward-outline"></ion-icon>'],
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 7000,
+            smartSpeed: 1000,
+            // responsive: {
+            //     0: {
+            //         items: 2
+            //     },
+            //     768: {
+            //         items: 3
+            //     },
+            //     992: {
+            //         items: 4
+            //     },
+            //     1200: {
+            //         items: 6
+            //     }
+            // }
+        });
+    }
+
+})(jQuery); 
---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+(function($){
+    
+    "use strict";
+    
+    
+    //===== Mobile Menu 
+    
+    $(".navbar-toggler").on('click', function() {
+        $(this).toggleClass('active');
+    });
+    
+    $(".navbar-nav a").on('click', function() {
+        $(".navbar-toggler").removeClass('active');
+    });
+    
+    
+    //===== close navbar-collapse when a  clicked
+    
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+    
+    
+    //===== Sticky
+    $(window).on('scroll', function(event) {    
+        var scroll = $(window).scrollTop();
+        if (scroll < 10) {
+            $(".navigation").removeClass("sticky");
+        } else{
+            $(".navigation").addClass("sticky");
+        }
+    });
+    
+    
+    //===== Section Menu Active
+    var scrollLink = $('.page-scroll');
+        // Active link switching
+        $(window).scroll(function() {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function() {
+
+          var sectionOffset = $(this.hash).offset().top - 73;
+
+          if ( sectionOffset <= scrollbarLocation ) {
+            $(this).parent().addClass('active');
+            $(this).parent().siblings().removeClass('active');
+          }
+        });
+    });
+    
+    
+    // Parallaxmouse js
+    function parallaxMouse() {
+        if ($('#parallax').length) {
+            var scene = document.getElementById('parallax');
+            var parallax = new Parallax(scene);
+        };
+    };
+    parallaxMouse();
+     
+}(jQuery));
